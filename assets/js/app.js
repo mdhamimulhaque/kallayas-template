b82ca7adbe6eb5246502e7bc6afb4d2930b7af55
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-04-18 12:35:51 +0600

    footer done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const open = document.querySelector(".open");
+const cardBanner = document.querySelector(".banner-card");
 
 open.addEventListener("click",()=>{
     close.style.display = "block";
+    cardBanner.style.display = "none";
     menu.classList.toggle("active");
 })
 
 close.addEventListener("click",()=>{
     close.style.display = "none";
+    cardBanner.style.display = "block";
     menu.classList.toggle("active");
 })
